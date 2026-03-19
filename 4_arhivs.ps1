389f554d08200093c421dfdc2b85eb106eacc73a
GirtRubenis <girtruben333@gmail.com>
2026-03-19 10:48:03 +0200

    Update 4_arhivs.ps1

--- 4_arhivs.ps1
+++ 4_arhivs.ps1
@@ -1,20 +1,7 @@
-﻿$downloadsPath = Join-Path "$env:USERPOFILE\Downloads"
-$documentsPath = Join-Path "$env:USERPROFILE\Documents\"PDF_Backup.zip"
+$source = "$env:USERPROFILE\Downloads"
+$dest = "$env:USERPROFILE\Documents\PDF_Backup.zip"
+$files = Get-ChildItem -Path $source -Filter *.pdf | Where-Object { $_.LastWriteTime -gt (Get-Date).AddHours(-48) }
 
-
-"
-$backupPath = Join-Path $documentsPath
-
-
-$limitDate = (Get-Date).AddHours(-48)
-
-$filesToBackup = Get-ChildItem -Path $downloadsPath -Filter "*.pdf" | Where-Object {
-    $_.LastWriteTime -gt $limitDate -or $_.CreationTime -gt $limitDate
-}
-
-if ($filesToBackup) {
-    Compress-Archive -Path $filesToBackup.FullName -DestinationPath $backupPath -Force
-    Write-Host "Rezerves kopija veiksmigi izveidota: $backupPath" -ForegroundColor Green
-} else {
-    Write-Host "Pedejo 48 stundu laika nav atrasts neviens jauns vai mainits PDF fails." -ForegroundColor Yellow
+if ($files) {
+Compress-Archive -Path $files.FullName -DestinationPath $dest -Force
 }
